179f869e1f76036287cda15f5a6b246e31644d31
Kiran Gautam <kgautam@oceanx.com>
2017-12-27 13:25:39 -0800

    adding zoom api token

--- api/controllers/walletController.js
+++ api/controllers/walletController.js
@@ -1,8 +1,7 @@
 'use strict';
 
-exports.get_wallet = function (req, res) {
+exports.get_wallet = function(req, res) {
     console.log("in wallet");
-
     var crypto = require('crypto');
     var request = require('request');
     var apiKey = 'xx';
@@ -29,7 +28,7 @@ exports.get_wallet = function (req, res) {
             'CB-VERSION': '2015-07-22'
         }
     };
-    request(options, function (err, response) {
+    request(options, function(err, response) {
         if (err) {
             console.log(err);
             res.status(404);
@@ -41,5 +40,23 @@ exports.get_wallet = function (req, res) {
             res.json(JSON.parse(response.body));
         }
     });
+};
+
+
+exports.get_token = function(req, res) {
+    console.log("in token");
+    var jwt = require('jsonwebtoken');
+
+    var payload = {
+        iss: "_7aGxkI0QSyWjIBgN5JEYw",
+        exp: ((new Date()).getTime() + 50000)
+    };
+
+    //Automatically creates header, and returns JWT
+    var token = jwt.sign(payload, "Tx57j2ZIOWfC2EqvRE5TMYELdnvVWS1YIVhY");
+    console.log("Zoom token created: "+ token);
+    res.setHeader('Content-Type', 'application/json');
+    res.status(200);
+    res.json({"token": token});
 
 };
--- api/routes/walletRoutes.js
+++ api/routes/walletRoutes.js
@@ -1,7 +1,9 @@
 'use strict';
 module.exports = function(app) {
-  var todoList = require('../controllers/walletController');
+    var todoList = require('../controllers/walletController');
 
     app.route('/wallet')
         .get(todoList.get_wallet);
+    app.route('/token')
+        .get(todoList.get_token);
 };
